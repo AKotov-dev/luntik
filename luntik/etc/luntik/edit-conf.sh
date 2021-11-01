017b8b0ecff93570a7bfe35f323a15dfbbaad5f0
AKotov-dev <alex_q_2000@mail.ru>
2021-11-01 09:42:14 +0300

    v0.1 simple openvpn connector

--- luntik/etc/luntik/edit-conf.sh
+++ luntik/etc/luntik/edit-conf.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Загрузка и редактирование конфигурации
-#if [[ -z $(find /etc/luntik/luntik.conf -mtime -3) ]]; then
-#curl -m 10 -o /etc/luntik/luntik.conf https://tools.vanwa.tech/download-free-openvpn-config >> /var/log/luntik.log 2>&1 &&
-
-systemctl stop luntik &&
+# Редактирование конфигурации и перезапуск
+systemctl stop luntik
 
 echo -e "\n"\
 "#DNS from VPN\n"\
@@ -12,8 +9,7 @@ echo -e "\n"\
 "up /etc/luntik/update-resolv-conf\n"\
 "down /etc/luntik/update-resolv-conf" >> /etc/luntik/luntik.conf
 sed -i 's/^persist-tun*/#persist-tun/' /etc/luntik/luntik.conf
-#fi;
 
 systemctl start luntik
 
-exit 0
+exit 0;
