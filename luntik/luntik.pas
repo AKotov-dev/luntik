e6c9169fd2beb1ed9e7f4fbde02b5234585075b4
AKotov-dev <alex_q_2000@mail.ru>
2022-10-30 20:54:05 +0300

    v0.8 - Release for Mageia-8 and Mageia-9

--- luntik/luntik.pas
+++ luntik/luntik.pas
@@ -14,7 +14,7 @@ uses
 
 begin
   RequireDerivedFormResource:=True;
-  Application.Title:='Luntik v0.6';
+  Application.Title:='Luntik v0.7';
   Application.Scaled:=True;
   Application.Initialize;
   Application.CreateForm(TMainForm, MainForm);
